bf99425cead91377e78270085abb272df913ac38
Tevinthuku <tevinthuku@gmail.com>
2025-01-07 08:53:46 +0300

    remove mocks on updatepage tests

--- backend/handlers/updatepage_test.go
+++ backend/handlers/updatepage_test.go
@@ -1,9 +1,8 @@
 package handlers_test
 
 import (
-	"encoding/json"
+	"go_notion/backend/db"
 	"go_notion/backend/handlers"
-	"go_notion/backend/mocks"
 	"go_notion/backend/router"
 	"net/http"
 	"net/http/httptest"
@@ -11,18 +10,20 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
-	"github.com/pashagolub/pgxmock/v4"
+	"github.com/gofrs/uuid/v5"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestUpdatePage(t *testing.T) {
-	mock, err := pgxmock.NewPool()
+
+	pageId := uuid.Must(uuid.NewV4())
+	pool, err := db.RunTestDb(db.InsertTestUserFixture, db.InsertTestPageFixture(pageId, 1))
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer mock.Close()
+	defer pool.Close()
 
-	updatePage, err := handlers.NewUpdatePageHandler(mock)
+	updatePage, err := handlers.NewUpdatePageHandler(pool)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -32,7 +33,6 @@ func TestUpdatePage(t *testing.T) {
 		userID         any
 		body           string
 		getPageID      func() string
-		setupMock      func(mock pgxmock.PgxPoolIface)
 		expectedStatus int
 	}{
 		{
@@ -40,12 +40,7 @@ func TestUpdatePage(t *testing.T) {
 			userID: int64(1),
 			body:   `{"title_text": "title", "content_text": "content", "raw_title": {"data": "title"}, "raw_content": {"data": "content"}}`,
 			getPageID: func() string {
-				return "d23d0a84-3260-4670-aa1f-5d316ba6325b"
-			},
-			setupMock: func(mock pgxmock.PgxPoolIface) {
-				mock.ExpectExec(`
-				UPDATE pages SET text_title = \$1, text_content = \$2, title = \$3, content = \$4 WHERE id = \$5 AND created_by = \$6
-			`).WithArgs("title", "content", json.RawMessage(`{"data": "title"}`), json.RawMessage(`{"data": "content"}`), mocks.AnyUUID{}, int64(1)).WillReturnResult(pgxmock.NewResult("UPDATE", 1))
+				return pageId.String()
 			},
 			expectedStatus: http.StatusOK,
 		},
@@ -56,8 +51,6 @@ func TestUpdatePage(t *testing.T) {
 			getPageID: func() string {
 				return "123e4567-e89b-12d3-a456-426614174000"
 			},
-			setupMock: func(mock pgxmock.PgxPoolIface) {
-			},
 			expectedStatus: http.StatusUnauthorized,
 		},
 		{
@@ -66,8 +59,6 @@ func TestUpdatePage(t *testing.T) {
 			body:   `{"title_text": "title", "content_text": "content", "raw_title": {"data": "title"}, "raw_content": {"data": "content"}}`,
 			getPageID: func() string {
 				return "invalid"
-			},
-			setupMock: func(mock pgxmock.PgxPoolIface) {
 			},
 			expectedStatus: http.StatusBadRequest,
 		},
@@ -78,16 +69,12 @@ func TestUpdatePage(t *testing.T) {
 			getPageID: func() string {
 				return "d23d0a84-3260-4670-aa1f-5d316ba6325b"
 			},
-			setupMock: func(mock pgxmock.PgxPoolIface) {
-			},
 			expectedStatus: http.StatusBadRequest,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			mock.Reset()
-			test.setupMock(mock)
 			r := router.NewRouter()
 
 			r.PUT("/api/pages/:id", func(c *gin.Context) {
